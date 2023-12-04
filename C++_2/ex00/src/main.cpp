cc0873cc55452a16fdb33a10a13b1309f390c323
TCFD <r.ciaze@gmail.com>
2023-12-04 14:48:28 +0100

    nouveau test

--- C++_2/ex00/src/main.cpp
+++ C++_2/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rciaze <rciaze@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 12:56:28 by zbp15             #+#    #+#             */
-/*   Updated: 2023/11/28 18:12:42 by rciaze           ###   ########.fr       */
+/*   Updated: 2023/12/04 14:47:18 by rciaze           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 int main( void ) {
 	Fixed a;
+	a.setRawBits( 10 );
 	Fixed b( a );
 	Fixed c;
-	c = b;
+	c = a;
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
